8a87eccf0b3b94e4ac08436f69307abf585a6477
Reyzal <reyzalgelarpratama@gmail.com>
2025-08-12 20:25:44 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MajorController;
+use App\Http\Controllers\ClassLevelController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Resource routes untuk semua tabel
+Route::resource('roles', RoleController::class);
+Route::resource('users', UserController::class);
+Route::resource('majors', MajorController::class);
+Route::resource('class-levels', ClassLevelController::class);
+Route::resource('subjects', SubjectController::class);
+Route::resource('teachers', TeacherController::class);
+Route::resource('schedules', ScheduleController::class);
+Route::resource('notifications', NotificationController::class);
